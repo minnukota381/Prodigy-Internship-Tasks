b4c07ef3c9712cf017c54acda963233ad929ec66
minnukota381 <minnukota381@gmail.com>
2024-04-21 13:53:34 +0530

    Task 4

--- PRODIGY_WD_04/script.js
+++ PRODIGY_WD_04/script.js
@@ -1,24 +1,23 @@
-// Sticky Navigation Menu JS Code
 let scrollBtn = document.querySelector(".scroll-button a");
 console.log(scrollBtn);
 let val;
 window.onscroll = function() {
   if (document.documentElement.scrollTop > 20) {
-    if (scrollBtn) { // Check if scrollBtn exists before using it
+    if (scrollBtn) { 
       scrollBtn.style.display = "block";
     }
   } else {
-    if (scrollBtn) { // Check if scrollBtn exists before using it
+    if (scrollBtn) { 
       scrollBtn.style.display = "none";
     }
   }
 }
 
-// Typed.js initialization
 let options = {
   strings: ["Full Stack Developer", "Web Developer", "Software Developer"],
   typeSpeed: 50,
   backSpeed: 50,
-  loop: true
+  loop: true,
+  cursorChar: "|",
 };
-let typed = new Typed("#typed-text", options);
+let typed = new Typed("#typed-text span", options);
